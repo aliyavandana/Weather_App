6f8cdb16a0aff64736923f041dad545edc14bc1a
aliyavandana <aliyavandana@users.noreply.github.com>
2020-06-20 11:10:33 +0530

    Created Form, Title and Weather Components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import Title from './Components/Title'
+import Weather from './Components/Weather'
+import Form from './Components/Form'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <Title />
+        <Weather />
+        <Form />
+      </div>
+    )
+  }
+} 
+
+
 
 export default App;
